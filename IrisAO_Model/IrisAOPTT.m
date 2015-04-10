2206f088f217781ba6918d1ef2c8ac85e4fd9ca1
atrodack <atrodack@email.arizona.edu>
2015-04-10 16:56:06 -0700

    Working IrisAO model being added

--- IrisAO_Model/IrisAOPTT.m
+++ IrisAO_Model/IrisAOPTT.m
@@ -1,89 +1,102 @@
 function [ DM ] = IrisAOPTT( DM, segList, pistonList, tipList, tiltList, bump)
-%[ DM ] = IrisAOPTT( DM, segList, pistonList, tipList, tiltList )
+%**************************************************************************
+%*********************************ANTIQUATED*******************************
+%**************************************************************************
+%****USE PTT METHOD IN AOAPERTURE INSTEAD---MUST MAP YOURSELF IF YOU DO****
+%**************************************************************************
+% [ DM ] = IrisAOPTT( DM, segList, pistonList, tipList, tiltList )
 %   Applys Piston, Tip, and Tilt to an IrisAO model. This will do it in
 %   hardware order, not in the Model's order. Input segList can be 1:37,
 %   and this will map to the corresponding segment in DM.segList.
 %
+% The file PTT111 Numbered Array in the folder is a labeled picture of the
+% hardware order of segments. Use this to assist you learn how the segments
+% are layed out (rings outward from the center).
+%
 % pistonList is in units of meters---> on order of microns
-% tipList and tiltList are in units of radians ----> on order of mrad
+% tipList and tiltList are in units of radians ----> on order of milliradians
 
-%% Check Inputs
+%% The Ugly Nargin Section....
+%all the fprintf's are commented because they take too much time to print
+%on screen. They are left here for debugging purposes or so they can be
+%turned on if desired
+
 if nargin == 1
     segList = 1:37;
     pistonList = zeros(37,1);
     tipList = zeros(37,1);
     tiltList = zeros(37,1);
     bump = false;
-    fprintf('Flattening the Mirror\n');
+%     fprintf('Flattening the Mirror\n');
 elseif nargin == 3
     tipList = zeros(37,1);
     tiltList = zeros(37,1);
     bump = false;
-    fprintf('Applying Piston Only\n');
+%     fprintf('Applying Piston Only\n');
 elseif nargin == 4
     tiltList = zeros(37,1);
     bump = false;
-    fprintf('Applying Piston and Tip\n');
+%     fprintf('Applying Piston and Tip\n');
 elseif nargin == 5
-    maxval1 = max(abs(pistonList));
-    maxval2 = max(abs(tipList));
-    maxval3 = max(abs(tiltList));
-    if maxval1 <= 1e-10
-        if maxval2 == 0
-            if maxval3 == 0
-                fprintf('Flattening the Mirror\n')
-            else
-                fprintf('Applying Piston and Tilt\n');
-            end
-        else
-            if maxval3 == 0
-                fprintf('Applying Piston and Tip\n');
-            else
-                fprintf('Applying Piston, Tip, and Tilt\n');
-            end
-        end
-        fprintf('Applying Piston\n');
-    end
+%     maxval1 = max(abs(pistonList));
+%     maxval2 = max(abs(tipList));
+%     maxval3 = max(abs(tiltList));
+%     if maxval1 <= 1e-10
+%         if maxval2 == 0
+%             if maxval3 == 0
+%                 fprintf('Flattening the Mirror\n')
+%             else
+%                 fprintf('Applying Piston and Tilt\n');
+%             end
+%         else
+%             if maxval3 == 0
+%                 fprintf('Applying Piston and Tip\n');
+%             else
+%                 fprintf('Applying Piston, Tip, and Tilt\n');
+%             end
+%         end
+%         fprintf('Applying Piston\n');
+%     end
     
     bump = false;
 elseif nargin == 6
-    maxval1 = max(abs(pistonList));
-    maxval2 = max(abs(tipList));
-    maxval3 = max(abs(tiltList));
-    if maxval1 <= 1e-10
-        if maxval2 == 0
-            if maxval3 == 0
-                fprintf('Flattening the Mirror\n')
-            else
-                fprintf('Applying Tilt\n');
-            end
-        else
-            if maxval3 == 0
-                fprintf('Applying Tip\n');
-            else
-                fprintf('Applying Tip and Tilt\n');
-            end
-        end
-    else
-        if maxval2 == 0
-            if maxval3 == 0
-                fprintf('Applying Piston\n')
-            else
-                fprintf('Applying Piston and Tilt\n');
-            end
-        else
-            if maxval3 == 0
-                fprintf('Applying Piston and Tip\n');
-            else
-                fprintf('Applying Piston, Tip, and Tilt\n');
-            end
-        end
-    end
-    if bump == true
-        fprintf('*****Bumping Segments*****\n');
-    else
-        fprintf('*****Overwriting Piston, Tip, and Tilt*****\n');
-    end
+%     maxval1 = max(abs(pistonList));
+%     maxval2 = max(abs(tipList));
+%     maxval3 = max(abs(tiltList));
+%     if maxval1 <= 1e-10
+%         if maxval2 == 0
+%             if maxval3 == 0
+%                 fprintf('Flattening the Mirror\n')
+%             else
+%                 fprintf('Applying Tilt\n');
+%             end
+%         else
+%             if maxval3 == 0
+%                 fprintf('Applying Tip\n');
+%             else
+%                 fprintf('Applying Tip and Tilt\n');
+%             end
+%         end
+%     else
+%         if maxval2 == 0
+%             if maxval3 == 0
+%                 fprintf('Applying Piston\n')
+%             else
+%                 fprintf('Applying Piston and Tilt\n');
+%             end
+%         else
+%             if maxval3 == 0
+%                 fprintf('Applying Piston and Tip\n');
+%             else
+%                 fprintf('Applying Piston, Tip, and Tilt\n');
+%             end
+%         end
+%     end
+%     if bump == true
+%         fprintf('*****Bumping Segments*****\n');
+%     else
+%         fprintf('*****Overwriting Piston, Tip, and Tilt*****\n');
+%     end
 else
     error('Number of input arguments is incorrect');
 end
@@ -93,47 +106,24 @@ if ~isa(DM,'AOAperture')
 end
 
 %% Load in the Mapping Vector
-% load('IrisAO_MAP.mat');
-load('IrisAO_STUFF.mat');
-figure;
+load('IrisAO_SegMap.mat');
 %% Do the Mapping and Apply PTT
 if bump == false
-    jj = 1;
     for ii = 1:37
-        while(jj <= 37)
-            if IrisAO_MAP(jj) == segList(ii)
-                mapped_segment = jj;
-                jj = 50;
-            else
-                jj = jj + 1;
-            end
-        end
-        
+        mapped_segment = IrisAO_SegMap(ii);
         DM.segList{ii}.piston = pistonList(mapped_segment);
         DM.segList{ii}.tiptilt = [tipList(mapped_segment),tiltList(mapped_segment)];
-        jj = 1;
     end
 else
-    jj = 1;
     for ii = 1:37
-        while(jj <= 37)
-            if IrisAO_MAP(jj) == segList(ii)
-                mapped_segment = jj;
-                jj = 50;
-            else
-                jj = jj + 1;
-            end
-        end
-        
+        mapped_segment = find(IrisAO_MAP == ii);
         DM.segList{ii}.piston = DM.segList{ii}.piston + pistonList(mapped_segment);
         DM.segList{ii}.tiptilt = DM.segList{ii}.tiptilt + [tipList(mapped_segment),tiltList(mapped_segment)];
-        jj = 1;
     end
 end
 
-%% Go Ahead and Render It
+%% Go Ahead and Render it
 DM.touch;
 DM.render;
-
 end
 
--- IrisAO_Model/makeIrisAODM.m
+++ IrisAO_Model/makeIrisAODM.m
@@ -1,7 +1,22 @@
-function [varargout] = makeIrisAODM(MAGNIFICATION, verbose)
-% [DM] = makeIrisAODM(MAGNIFICATION, verbose, Scalloped_Field(when debugged))
-% Makes a 37 Segment IrisAO DM. 
-% Magnification scales the DM, Scalloped_Field also returns a Scalloped Field
+function [varargout] = makeIrisAODM(MAGNIFICATION, verbose,Scalloped_Field)
+% [DM] = makeIrisAODM(MAGNIFICATION, verbose, Scalloped_Field)
+% Makes a 37 Segment IrisAO DM.
+%
+%Inputs:
+% MAGNIFICATION scales the DM Segment Size. SegPitch of 606 microns is
+% hardcoded because it is what standard IrisAO Mirrors use. If your mirror
+% is larger, MAGNIFICATION will scale that SegPitch accordingly.
+%
+% verbose turns on/off plotting various things as the construction takes
+% place. You should set it to true once....it is really cool.
+%
+% Scalloped_Field turns on returning an AOField Object that encodes the
+% actual surface shape of the segments.
+%
+%Output:
+% DM is an AOAperture class object that has 37 segments stored within it.
+% This is the IrisAO Model
+%
 
 SegPitch = 606e-6;
 
@@ -9,37 +24,38 @@ if nargin == 0
     MAGNIFICATION = 1;
     SegPitch = MAGNIFICATION * 606e-6;
     verbose = true;
-%     Scalloped_Field = false;
+    Scalloped_Field = false;
 elseif nargin == 1
     SegPitch = MAGNIFICATION * SegPitch;
-%     Scalloped_Field = false;
+    Scalloped_Field = false;
     verbose = false;
 elseif nargin == 2
     SegPitch = MAGNIFICATION * SegPitch;
-%     Scalloped_Field = false;
-% elseif nargin == 3
-%     SegPitch = MAGNIFICATION * SegPitch;
+    Scalloped_Field = false;
+elseif nargin == 3
+    SegPitch = MAGNIFICATION * SegPitch;
 else
     error('Inputs are Incorrect');
 end
 
+% set the grid spacing
 dx = SegPitch/100;
+% set the gap width
 GAP = SegPitch*0.02;
 
+% set up a segment
 Seg = AOSegment(ceil((SegPitch+2*GAP)/dx*1.25));
 Seg.spacing(dx);
 
 SCALLOPING = 100e-9;
 
+% Grab the coordinate systems
 [x,y] = Seg.coords;
 [X,Y] = Seg.COORDS;
 
-% Q = (SegPitch/2)*exp(2*pi*1i*(0:6)'/6);
-% V = [real(Q) imag(Q)];
-% dV = diff(V,1,1);
-
 SEG = 1;
 
+% Make a Hexagon
 for n=1:6
     th = 60/180*pi*n;
     SEG=SEG.*smoothedge(SegPitch/2- (X*cos(th)-Y*sin(th)),dx);
@@ -51,13 +67,17 @@ for n=1:6
     end
 end
 
+%Set the Segment Grid to the hexagon
 Seg.grid(SEG);
 Seg.name = 'IrisAO DM Segment';
 
+%Initialize the Model
 DM = AOAperture();
 Seg.name = 'IrisAO DM';
 DM.spacing(dx);
 
+% Some important numbers, and some unit vectors to point to where the
+% segments need to be placed as they are constructed
 a2 = (SegPitch/2+GAP)*1.333*1.0;
 a1 = a2*sqrt(3)/2;
 
@@ -67,7 +87,11 @@ c = cos(th1); s = sin(th1);
 ub = ([c -s; s c] * ua')';
 ub_ = ([c s; -s c] * ua')';
 
-
+% Build the DM, start at bottom row, and add in segments sweeping from left
+% to right. The axis of symmetry for the rows is row #0. Below this, ub is
+% used, and above this ub_ is used.  These unit vectors account for the
+% necessary sign change in the rotation now that the segments are in
+% positive space vertically.
 for nrow=-3:0
     NumCol = 7-abs(nrow);
     
@@ -95,45 +119,46 @@ for nrow=1:3
     end
 end
 
-
+% Store the location of the Center of each Segment in the global coordinate
+% system
 SEGCOORDS = zeros(37,2);
 for n=1:37
     SEGCOORDS(n,:) = DM.segList{n}.Offset;
 end
+
+%return the DM Model
 varargout{1} = DM;
 
 
-
-% DOESN'T WORK RIGHT NOW
-% %% Scalloping on Field
-% if Scalloped_Field == true;
-%     F = AOField(DM);
-%     F.name = 'Scalloped Field';
-%     F.lambda = AOField.VBAND;
-%     F.FFTSize = 1024*2;
-%     
-%     DM.lambdaRef = F.lambda;
-%     
-%     DM.touch;
-%     
-%     PS = AOScreen(DM);
-%     [X,Y] = PS.COORDS;
-%     
-%     PS.zero;
-%     w = SegPitch/2.2;
-%     
-%     for n=1:length(DM.segList)
-%         LOC = DM.segList{n}.Offset;
-%         R = sqrt((X-LOC(2)+SegPitch).^2+(Y-LOC(1)).^2); % Kludge!
-%         PS.grid(PS.grid+exp(-(R/w).^2));
-%     end
-%     
-%     PS.grid(SCALLOPING*normalize(PS.grid-min(PS.grid_(:))));
-%     
-%     F.planewave*DM*PS;
-%     if verbose == true;
-%         F.show;
-%     end
-%     varargout{2} = F;
-% end
+%% Scalloping on Field
+if Scalloped_Field == true;
+    F = AOField(DM);
+    F.name = 'Scalloped Field';
+    F.lambda = AOField.VBAND;
+    F.FFTSize = 1024*2;
+    
+    DM.lambdaRef = F.lambda;
+    
+    DM.touch;
+    
+    PS = AOScreen(DM);
+    [X,Y] = PS.COORDS;
+    
+    PS.zero;
+    w = SegPitch/2.2;
+    
+    for n=1:length(DM.segList)
+        LOC = DM.segList{n}.Offset;
+        R = sqrt((X-LOC(2)).^2+(Y-LOC(1)).^2);
+        PS.grid(PS.grid+exp(-(R/w).^2));
+    end
+    
+    PS.grid(SCALLOPING*normalize(PS.grid-min(PS.grid_(:))));
+    
+    F.planewave*DM*PS;
+    if verbose == true;
+        F.show;
+    end
+    varargout{2} = F;
+end
 end
